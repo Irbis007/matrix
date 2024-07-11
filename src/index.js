fa387b7bf1626f63def72bdf8372b4a2a9e1a632
Irbis007 <suskodomk@gmail.com>
2024-07-11 03:02:24 +0300

    add console.log

--- src/index.js
+++ src/index.js
@@ -7,15 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const container = document.querySelector('.container')
 
-<<<<<<< HEAD
-=======
-  console.log(1)
-
->>>>>>> 1e5a6ce (add calculators result)
 
   const menuHamburger = document.querySelector('.menu__hamburger')
   const menu = document.querySelector('.header__menu')
-
+  console.log(2)
   menuHamburger.addEventListener('click', () => {
     menuHamburger.classList.toggle('menu__hamburger_active')
     menu.classList.toggle('header__menu_active')
@@ -23,13 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const faqItems = document.querySelectorAll('.faq__body .item')
 
-<<<<<<< HEAD
-  faqItems.forEach((item, i) => {
-    item.addEventListener('click', () =>{
-      item.classList.toggle('active')
-    })
-  })
-=======
   if(faqItems) {
     faqItems.forEach((item, i) => {
       item.addEventListener('click', () =>{
@@ -37,28 +25,11 @@ document.addEventListener('DOMContentLoaded', () => {
       })
     })
   }
->>>>>>> 1e5a6ce (add calculators result)
 
   const infoItem = document.querySelectorAll('.info__item');
   const infoItemStatus = document.querySelectorAll('.info__item-top__text')
   const infoItemImg = document.querySelectorAll('.info__item-top__img')
 
-<<<<<<< HEAD
-  infoItem.forEach((item, i) => {
-    item.addEventListener('click', (e) => {
-      if(!item.classList.contains('info__item_disabled') && !e.target.closest('.info__item-body' ) && e.target.closest('.info__item-top')){
-        item.classList.toggle('info__item_active')
-      }
-    })
-    if(item.classList.contains('info__item_disabled')){
-      infoItemStatus[i].textContent = 'Блок недоступен';
-      
-      infoItemImg[i].innerHTML = `
-        <img src="./images/icons/block-close.png">
-      `;
-    }
-  })
-=======
   if(infoItem){
     infoItem.forEach((item, i) => {
       item.addEventListener('click', (e) => {
@@ -75,31 +46,12 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
   }
->>>>>>> 1e5a6ce (add calculators result)
 
   
 
   const tabButtons = document.querySelectorAll('.info__item-tab__button')
   const tabContents = document.querySelectorAll('.info__item-tab__content')
 
-<<<<<<< HEAD
-  tabButtons.forEach((item, i) => {
-    item.addEventListener('click', () => {
-      removeClass(tabButtons, 'info__item-tab__content_active')
-      removeClass(tabContents, 'info__item-tab__button_active')
-      tabContents[i].classList.add('info__item-tab__content_active')
-      item.classList.add('info__item-tab__button_active')
-    })
-  })
-
-  const selectMale = document.querySelectorAll('.select__male-selected');
-
-  selectMale.forEach((item, i) => {
-    item.addEventListener('click', () => {
-      item.parentElement.classList.toggle('select__male-selected_active')
-    })
-  })
-=======
   if(tabButtons){
     tabButtons.forEach((item, i) => {
       item.addEventListener('click', () => {
@@ -120,7 +72,6 @@ document.addEventListener('DOMContentLoaded', () => {
       })
     })
   }
->>>>>>> 1e5a6ce (add calculators result)
 
   const selectMaleRadio = document.querySelectorAll('.select__male-radio')
   const selectMaleText = document.querySelectorAll('.select__male-selected__text')
@@ -144,16 +95,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const infoItemHelp = document.querySelectorAll('.info__item-help')
 
-<<<<<<< HEAD
-  infoItemStatus.forEach((item, i) => {
-    item.addEventListener('mouseover', () =>{
-      infoItemHelp[i].classList.add('info__item-help_active')
-    })
-    item.addEventListener('mouseout', () =>{
-      infoItemHelp[i].classList.remove('info__item-help_active')
-    })
-  })
-=======
   if(infoItemHelp){
     infoItemStatus.forEach((item, i) => {
       item.addEventListener('mouseover', () =>{
@@ -164,7 +105,6 @@ document.addEventListener('DOMContentLoaded', () => {
       })
     })
   }
->>>>>>> 1e5a6ce (add calculators result)
 
   const authLoginButton = document.querySelector('.auth__login')
   const authRegisterButton = document.querySelector('.auth__register')
